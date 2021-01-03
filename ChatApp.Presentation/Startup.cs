45bbd6ff87d4f2c36bf2fd5d76c0827840c5600f
Erhan Alankus <e.alankus@gmail.com>
2021-01-03 13:23:41 +0300

    Allow Weak Passwords

--- ChatApp.Presentation/Startup.cs
+++ ChatApp.Presentation/Startup.cs
@@ -33,6 +33,17 @@ namespace ChatApp.Presentation
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+
+            // Allow weak passwords.
+            services.Configure<IdentityOptions>(options =>
+            {                
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequiredUniqueChars = 0;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
