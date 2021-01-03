b5458b829d62e3e49e3f509d7a1cb37c579ac8db
Erhan Alankus <e.alankus@gmail.com>
2021-01-03 13:53:06 +0300

    Add First Name and Last Name to "Register" and "Manage Account" Pages

---
+++ ChatApp.Presentation/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ChatApp.Presentation.Data;
+using ChatApp.Presentation.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ChatApp.Presentation.Areas.Identity.IdentityHostingStartup))]
+namespace ChatApp.Presentation.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
