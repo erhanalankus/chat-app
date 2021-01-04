ad005d229cebaf18f1f67ac5b38c2af0f708e3ba
Erhan Alankus <e.alankus@gmail.com>
2021-01-04 12:50:47 +0300

    Add Static Resources for Front End

--- ChatApp.Presentation/wwwroot/js/site.js
+++ ChatApp.Presentation/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(function () {
+    $('ul#users-list').on('click', 'li', function () {
+        var username = $("input[type=hidden].username", $(this)).val();
+        var input = $('#chat-message');
+
+        var text = input.val();
+        if (text.startsWith("/")) {
+            text = text.split(")")[1];
+        }
+
+        text = "/private(" + username + ") " + text.trim();
+        input.val(text);
+        input.change();
+        input.focus();
+    });
+
+    $('#emojis-container').on('click', 'a', function () {
+        var value = $("input", $(this)).val();
+        var input = $('#chat-message');
+        input.val(input.val() + value);
+        input.focus();
+        input.change();
+    });
+
+    $("#emojibtn").click(function () {
+        $("#emojis-container").toggleClass("d-none");
+    });
+
+    $("#chat-message, #btn-send-message").click(function () {
+        $("#emojis-container").addClass("d-none")
+    });
+
+    $('.modal').on('hidden.bs.modal', function () {
+        $(".modal-body input").val("");
+    });
+
+    $(".alert .close").on('click', function () {
+        $(this).parent().hide();
+    });
+});
