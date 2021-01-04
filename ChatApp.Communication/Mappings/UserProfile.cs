71e6508fe582f01c5bb8366e5faa700b6f75c969
Erhan Alankus <e.alankus@gmail.com>
2021-01-04 15:51:19 +0300

    Clean Up Using Statements

--- ChatApp.Communication/Mappings/UserProfile.cs
+++ ChatApp.Communication/Mappings/UserProfile.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using ChatApp.Core.Entities;
 using ChatApp.Core.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChatApp.Communication.Mappings
 {
--- ChatApp.Data/Configurations/RoomConfiguration.cs
+++ ChatApp.Data/Configurations/RoomConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using ChatApp.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChatApp.Data.Configurations
 {
