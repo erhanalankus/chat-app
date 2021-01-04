775d1750d4e0e7c36e9a9b0ee23f81207bf761f1
Erhan Alankus <e.alankus@gmail.com>
2021-01-04 12:33:14 +0300

    Add NotMapped Full Name Property to Application User

--- ChatApp.Core/Entities/ApplicationUser.cs
+++ ChatApp.Core/Entities/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ namespace ChatApp.Core.Entities
         public string FirstName { get; set; }
         [PersonalData]
         public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => FirstName + " " + LastName;
 
         public ICollection<Room> Rooms { get; set; }
         public ICollection<Message> Messages { get; set; }
