f7c372f91a255dcd0b7b9b810c8f885163e2d098
Erhan Alankus <e.alankus@gmail.com>
2021-01-05 13:29:03 +0300

    Add Exception Message to the Error Page

--- ChatApp.Presentation/Pages/Error.cshtml.cs
+++ ChatApp.Presentation/Pages/Error.cshtml.cs
@@ -1,3 +1,5 @@
+using ChatApp.Core.CustomExceptions;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -6,11 +8,12 @@ using System.Diagnostics;
 namespace ChatApp.Presentation.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string ExceptionMessage { get; set; }
 
         private readonly ILogger<ErrorModel> _logger;
 
@@ -19,9 +22,23 @@ namespace ChatApp.Presentation.Pages
             _logger = logger;
         }
 
+        private void AddExceptionMessageToModel()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            _logger.LogError(ExceptionMessage);
+        }
+
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            AddExceptionMessageToModel();
+        }
+
+        public void OnPost()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            AddExceptionMessageToModel();
         }
     }
 }
