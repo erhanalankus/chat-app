f5d568273cd60cfe6b2acb71f85aa807fba5f4db
Erhan Alankus <e.alankus@gmail.com>
2021-01-08 10:32:10 +0300

    Fix InvalidSendgridCredentialsException Message

--- ChatApp.Core/CustomExceptions/InvalidSendgridCredentialsException.cs
+++ ChatApp.Core/CustomExceptions/InvalidSendgridCredentialsException.cs
@@ -12,7 +12,7 @@ namespace ChatApp.Core.CustomExceptions
         public InvalidSendgridCredentialsException(string sendgridUsername)
             : base(String.Format("Invalid SendGrid credentials. This is the SendGrid username that you supplied: {0}." +
                 " Please see the README for information on how to provide SendGrid credentials." +
-                "You can contact me at +905553989095 for my SendGrid credentials.", sendgridUsername))
+                "You can contact me at erhanalankus@gmail.com for support.", sendgridUsername))
         {
 
         }
